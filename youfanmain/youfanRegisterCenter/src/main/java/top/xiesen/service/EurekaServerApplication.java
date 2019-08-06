b636fc37f9372db51403dc8127ed4f39bfb9b1a0
xiesen110 <xiesen310@qq.com>
2019-08-06 09:16:33 +0800

    <dev> 用户画像之注册中心

---
+++ youfanmain/youfanRegisterCenter/src/main/java/top/xiesen/service/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package top.xiesen.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description 注册中心入口类
+ * @Author 谢森
+ * @Date 2019/8/6 0:31
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
