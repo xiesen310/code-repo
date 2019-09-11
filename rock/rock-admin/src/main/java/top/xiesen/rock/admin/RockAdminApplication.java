e7667a09ed87f68b8464dba805cf620f13d6a556
谢森 <xiesen310@163.com>
2019-09-11 17:16:49 +0800

    <dev> 磐石项目初始化

---
+++ rock/rock-admin/src/main/java/top/xiesen/rock/admin/RockAdminApplication.java
@@ -0,0 +1,13 @@
+package top.xiesen.rock.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RockAdminApplication.class, args);
+    }
+
+}
---
+++ rock/rock-common/src/main/java/top/xiesen/rock/App.java
@@ -0,0 +1,10 @@
+package top.xiesen.rock;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
