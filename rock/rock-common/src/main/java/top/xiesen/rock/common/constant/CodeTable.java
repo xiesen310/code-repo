2d6448b7518d0e93894ae55f373463d162d3548e
谢森 <xiesen310@163.com>
2019-09-12 17:32:11 +0800

    <dev> 添加rock-common 字符串处理方法

---
+++ rock/rock-common/src/main/java/top/xiesen/rock/common/constant/CodeTable.java
@@ -0,0 +1,46 @@
+package top.xiesen.rock.common.constant;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 业务处理状态码定义基本类
+ */
+public class CodeTable {
+    public static final Map<Integer, String> CODE_TABLE = new HashMap<>(1000);
+    /**
+     * 通用模块
+     */
+    public final static int SUCCESS = 0;
+    public final static int FAILURE = 1;
+
+    /**
+     * 参数相关
+     */
+    public final static int PARAM_ERR = 100100;
+    public final static int PARAM_FORMAT_ERR = 100101;
+    public final static int PARAM_JSON_ERR = 100102;
+
+
+    static {
+        CODE_TABLE.put(SUCCESS, "正常");
+        CODE_TABLE.put(FAILURE, "未知异常");
+
+        /**
+         * 100100-100199的错误码为输入参数相关问题
+         */
+        CODE_TABLE.put(PARAM_ERR, "参数异常");
+        CODE_TABLE.put(PARAM_FORMAT_ERR, "参数格式错误");
+        CODE_TABLE.put(PARAM_JSON_ERR, "Json参数解析失败");
+    }
+
+    /**
+     * 获取对用 code 的描述信息
+     *
+     * @param code 错误码
+     * @return 错误描述信息
+     */
+    public static String getDescription(Integer code) {
+        return CODE_TABLE.get(code);
+    }
+}
