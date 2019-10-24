be8eee6a8884602a5dfb36ddc1a22e4ec4c6da2c
谢森 <xiesen310@163.com>
2019-10-24 14:04:29 +0800

    <dev> 删除测试的用户CRUD

--- rock-verify/src/main/java/top/xiesen/verify/mapper/SysUserMapper.java
+++
@@ -1,7 +0,0 @@
-package top.xiesen.verify.mapper;
-
-import top.xiesen.verify.pojo.SysUser;
-import top.xiesen.verify.utils.MyMapper;
-
-public interface SysUserMapper extends MyMapper<SysUser> {
-}
--- rock-verify/src/main/java/top/xiesen/verify/mapper/SysUserMapperCustom.java
+++
@@ -1,9 +0,0 @@
-package top.xiesen.verify.mapper;
-
-import top.xiesen.verify.pojo.SysUser;
-
-import java.util.List;
-
-public interface SysUserMapperCustom {
-    List<SysUser> queryUserSimplyInfoById(String id);
-}
