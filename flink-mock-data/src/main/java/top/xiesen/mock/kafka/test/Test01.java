2c6b146f393ade0ef4b669bfe666d036b6126c3e
谢森 <xiesen310@163.com>
2020-04-20 14:50:34 +0800

    <dev> 正则表达式过滤数据

--- flink-mock-data/src/main/java/top/xiesen/mock/kafka/test/Test01.java
+++ flink-mock-data/src/main/java/top/xiesen/mock/kafka/test/Test01.java
@@ -9,26 +9,42 @@ package top.xiesen.mock.kafka.test;
  */
 public class Test01 {
     public static void main(String[] args) {
-        String a = "filter {\n" +
-                "  ruby {\n" +
-                "    code => \"\n" +
-                "     message = event.get('message')\n" +
-                "     time = message.split(' ')[0]\n" +
-                "     method = message.split(' ')[1]\n" +
-                "     type = message.split(' ')[2]\n" +
-                "     className = message.split(' ')[3]    \n" +
-                "     \n" +
-                "     event.set('time',time)\n" +
-                "     event.set('method',method)\n" +
-                "     event.set('type',type)\n" +
-                "     event.set('className',className)\n" +
-                "      \n" +
-                "    \"\n" +
+        String a = "CREATE TABLE myTable(\n" +
+                "\tlogTypeName varchar,\n" +
+                "\ttimestamp varchar,\n" +
+                "\tsource varchar,\n" +
+                "\toffset varchar,\n" +
+                "\tlatence double,\n" +
+                "\tid varchar,\n" +
+                "\tmessage varchar,\n" +
+                "\thostname varchar,\n" +
+                "\tappprogramname varchar,\n" +
+                "\tappsystem varchar\n" +
+                ")WITH(\n" +
+                "\ttype='kafka11',\n" +
+                "\tbootstrapServers='zorkdata-95:9092',\n" +
+                "\tzookeeperQuorum='zorkdata-91:2181/kafka111,zorkdata-92:2181/kafka111,zorkdata-95:2181/kafka111',\n" +
+                "\tkafka.key.deserializer='org.apache.kafka.common.serialization.StringDeserializer',\n" +
+                "\tkafka.value.deserializer='org.apache.kafka.common.serialization.ByteArrayDeserializer',\n" +
+                "\toffsetReset='earliest',\n" +
+                "\tgroupId='streamx_sql_01',\n" +
+                "\ttopic='streamx_sql_source',\n" +
+                "\tsourceDataType='logavro',\n" +
+                "\tparallelism='1',\n" +
+                "\tschemaString='hostname,appprogramname,appsystem|latence|id,message'\n" +
+                ");\n" +
                 "\n" +
-                "  }\n" +
+                "create view myResult(\n" +
+                "\tlatence double,\n" +
+                "\tid varchar,\n" +
+                "\tmessage varchar,\n" +
+                "\thostname varchar,\n" +
+                "\tappprogramname varchar,\n" +
+                "\tappsystem varchar\n" +
+                ");\n" +
                 "\n" +
-                "}\n";
+                "insert into myResult select latence,id,message,hostname,appprogramname,appsystem from myTable;";
 
-        System.out.println(a);
+        System.out.println(a.replaceAll("\n", "").replaceAll("\t", ""));
     }
 }
